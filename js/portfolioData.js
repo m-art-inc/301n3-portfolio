c012a6e6330d725a875d7c58717f6a2e825ace68
m-art-inc <martincarmen03@gmail.com>
2016-09-11 22:31:52 -0700

    replaced double quotation marks for single quotation signs

--- js/portfolioData.js
+++ js/portfolioData.js
@@ -1,26 +1,26 @@
 var portfolioItems = [
     {
-        title: "My first portfolio piece",
-        date: "2016-08-15",
+        title: 'My first portfolio piece',
+        date: '06-03-2016',
         url: '<button class="btn btn-default btn-lg"><a href="http://martincarmen.com">View Project</a></button>',
-        gitUrl: "https://www.github.com/m-art-inc",
-        description: "This is the first piece for my online portfolio. I created this during my coursework at Code Fellows",
+        gitUrl: 'https://www.github.com/m-art-inc',
+        description: 'This is the first piece for my online portfolio. I created this during my coursework at Code Fellows',
         imgUrl: '<img src="img/macbook.png" alt="macbook" />'
     },
     {
-        title: "My Second portfolio piece",
-        date: "2016-01-14",
+        title: 'My Second portfolio piece',
+        date: '08-20-2016',
         url: '<button class="btn btn-default btn-lg"><a href="http://martincarmen.com">View Project</a></button>',
-        gitUrl: "https://www.github.com/m-art-inc",
-        description: "This is the first piece for my online portfolio. I created this during my coursework at Code Fellows",
+        gitUrl: 'https://www.github.com/m-art-inc',
+        description: 'This is the first piece for my online portfolio. I created this during my coursework at Code Fellows',
         imgUrl: '<img src="img/macbook.png" alt="macbook" />'
     },
     {
-        title: "My Third portfolio piece",
-        date: "2016-03-15",
+        title: 'My Third portfolio piece',
+        date: '07-16-2016',
         url: '<button class="btn btn-default btn-lg"><a href="http://martincarmen.com">View Project</a></button>',
-        gitUrl: "https://www.github.com/m-art-inc",
-        description: "This is the first piece for my online portfolio. I created this during my coursework at Code Fellows",
+        gitUrl: 'https://www.github.com/m-art-inc',
+        description: 'This is the first piece for my online portfolio. I created this during my coursework at Code Fellows',
         imgUrl: '<img src="img/macbook.png" alt="macbook" />'
     }
 ];
