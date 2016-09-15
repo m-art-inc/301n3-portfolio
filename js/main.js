0af76c2c3c5f4818fdf3b75cc3b4eb4457e15739
m-art-inc <martincarmen03@gmail.com>
2016-09-14 18:18:18 -0700

    deleted un-needed code

--- js/main.js
+++ js/main.js
@@ -10,23 +10,6 @@ function Piece(keys) {
     this.imgUrl = keys.imgUrl;
 }
 
-// Piece.prototype.postIt = function(){
-//     var $newPiece = $('#templateDiv').clone(); //this creates a clone to save all styling and formatting
-//     $newPiece.append('<br>');
-//     $newPiece.find(".pieceTitle").html(this.title);
-//     // $newPiece.find('#sectionWrapper a').find('');
-//     $newPiece.find(".description p").html(this.description);
-//     $newPiece.find(".dateTime").html(this.date);
-//     $newPiece.find(".data-url a").html(this.url);
-//     $newPiece.find(".imgUrl").html(this.imgUrl);
-//     $newPiece.append('<br>','<br>','<br>');
-//
-//     $newPiece.removeClass('main-template');
-//
-//     return $newPiece;
-// };
-
-
 Piece.prototype.postIt = function() {
 
     var newPiece = $('#template').html();
