f7aa1981f84f6b8620c59742bb4ef8fd68eee1f8
m-art-inc <martincarmen03@gmail.com>
2016-10-04 16:32:18 -0700

    added routes function

---
+++ scripts/routes.js
@@ -0,0 +1,18 @@
+page("/", function() {
+  Piece.fetchData(portfolioView.initIndexPage),
+  aboutController.index();
+}),
+
+  page("/work", function() {
+    workController.index();
+  }),
+
+  page("/about", function() {
+    aboutController.index();
+  }),
+
+  page("/contact", function() {
+    contactController.index();
+  }),
+
+  page();
